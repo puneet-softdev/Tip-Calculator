4393c82f9a6feb9188d28899fa2299781245bd5b
Puneet Grover <ptgr.mail@gmail.com>
2021-11-28 20:46:25 +0530

    Tip Calculator Starter Project

---
+++ app/src/main/java/com/raywenderlich/android/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.raywenderlich.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.raywenderlich.android.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
